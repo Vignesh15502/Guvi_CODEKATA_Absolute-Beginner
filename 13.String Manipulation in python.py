34482d1d6b8d39836262de85712bedc5a1c0ac83
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-04 17:14:01 +0530

    Add files via upload

---
+++ 13.String Manipulation in python.py
@@ -0,0 +1,16 @@
+#Strint Manipulation
+
+'''
+ s  a  m  p  l  e
+ 0  1  2  3  4  5
+-6 -5 -4 -3 -2 -1 
+'''
+s="sample"            "outputs"
+print(s)               'sample'
+print(s[0:2])          'sa'  
+print(s[:5])           'sampl' 
+print(s[1:])           'ample'
+print(s[-1])           'e'
+print(s[-2:-1])        'l'
+print(s[:-1])          'sampl'
+print(s[::-1])         'elpmas'
