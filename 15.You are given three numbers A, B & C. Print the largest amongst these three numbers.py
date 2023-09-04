45988fc512f0cd83cecd0bf228382ad32d1cb076
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-04 17:23:48 +0530

    Add files via upload

---
+++ 15.You are given three numbers A, B & C. Print the largest amongst these three numbers.py
@@ -0,0 +1,22 @@
+'''
+You are given three numbers A, B & C. Print the largest amongst these three numbers.
+
+Input Description:
+Three numbers are provided to you.
+
+Output Description:
+Find and print the largest among the three
+
+Sample Input :
+1
+2
+3
+Sample Output :
+3
+'''
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=(a,b,c)
+print(max(d))
