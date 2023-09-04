db356c99673329fd7720106b435669355b5eff0c
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-04 17:24:43 +0530

    Add files via upload

---
+++ 17.Write a code to get an integer N and print the digits of the integer.py
@@ -0,0 +1,18 @@
+'''
+Write a code to get an integer N and print the digits of the integer.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the digits of the integer in a single line separated by space,
+
+Sample Input :
+348
+Sample Output :
+3 4 8
+'''
+
+a=input()
+b=' '.join(a)
+print(b)
