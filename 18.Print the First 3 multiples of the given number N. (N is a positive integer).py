c0d14f0d4b836bc21b713c49ff12d220bf05b048
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-04 17:25:03 +0530

    Add files via upload

---
+++ 18.Print the First 3 multiples of the given number N. (N is a positive integer).py
@@ -0,0 +1,19 @@
+'''
+Print the First 3 multiples of the given number "N". (N is a positive integer)
+
+Note: print the characters with a single space between them.
+
+Input Description:
+A positive integer is provided to you as an input.
+
+Output Description:
+Print the First 3 multiples of the number with single spaces between them as an output.
+
+Sample Input :
+2
+Sample Output :
+2 4 6
+'''
+
+a=int(input())
+print(a,a*2,a*3)
