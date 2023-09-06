2b8faa97c8a9771f751321cd8a62ef720d68aff8
VIGNESH P <142451424+Vignesh15502@users.noreply.github.com>
2023-09-06 17:00:24 +0530

    Add files via upload

---
+++ 22.Write a code to get an integer N and print values from 1 till N in a separate line.py
@@ -0,0 +1,22 @@
+'''
+Write a code to get an integer N and print values from 1 till N in a separate line.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the values from 1 to N in a separate line.
+
+Sample Input :
+5
+Sample Output :
+1
+2
+3
+4
+5
+'''
+
+a=int(input())
+for i in range(1,a+1):
+    print(i)
